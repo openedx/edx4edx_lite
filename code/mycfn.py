6531df3ef6107dc3272e5af2a531f2b059da6a5a
Aarif <mraarif@outlook.com>
2021-01-18 16:42:54 +0500

    modernized the repo

--- code/mycfn.py
+++ code/mycfn.py
@@ -1,43 +1,51 @@
-import pylab
-import base64, urllib, StringIO
+import StringIO
+import base64
+import six.moves.urllib.error
+import six.moves.urllib.parse
+import six.moves.urllib.request
 
-def test_add_to_ten(expect,ans):
-  a1=float(ans[0])
-  a2=eval(ans[1])
-  x = math.sqrt(2)
-  y = [x for x in range(10)]
-  return (a1+a2)==10
+import pylab
+
+
+def test_add_to_ten(expect, ans):
+    a1 = float(ans[0])
+    a2 = eval(ans[1])
+    x = math.sqrt(2)
+    y = [x for x in range(10)]
+    return (a1 + a2) == 10
+
 
 def test_add_to_x(expect, ans, options=10):
-  a1=float(ans[0])
-  a2=float(ans[1])
-  ok = (a1+a2)==int(options if options is not None else 10)
-  return dict(ok=ok, msg="options=%s" % options)
+    a1 = float(ans[0])
+    a2 = float(ans[1])
+    ok = (a1 + a2) == int(options if options is not None else 10)
+    return dict(ok=ok, msg="options=%s" % options)
+
 
 def test_plot(expect, ans, options=0):
-  xd = pylab.linspace(0,1,100)
-  expr = '[ %s for x in xd]' % ans
-  try:
-    yd = eval(expr)
-  except Exception as err:
-    msg = "Sorry, cannot evaluate your expression, err=%s" % str(err).replace('<','&lt;')
-    return dict(ok=False, msg=msg)
-  imgdata = StringIO.StringIO()
+    xd = pylab.linspace(0, 1, 100)
+    expr = '[ %s for x in xd]' % ans
+    try:
+        yd = eval(expr)
+    except Exception as err:
+        msg = "Sorry, cannot evaluate your expression, err=%s" % str(err).replace('<', '&lt;')
+        return dict(ok=False, msg=msg)
+    imgdata = StringIO.StringIO()
 
-  fig = pylab.figure()
-  ax = fig.add_subplot(111)
-  ax.plot(xd, yd, 'ro')
-  ax.plot(xd, yd)
-  ax.grid()
-  fig.savefig(imgdata, format='png')
-  pylab.close()
+    fig = pylab.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(xd, yd, 'ro')
+    ax.plot(xd, yd)
+    ax.grid()
+    fig.savefig(imgdata, format='png')
+    pylab.close()
 
-  imgdata.seek(0)  # rewind the data
-  uri = 'data:image/png;base64,' + urllib.quote(base64.b64encode(imgdata.buf))
-  msg = '<html><img src = "%s"/>' % uri
+    imgdata.seek(0)  # rewind the data
+    uri = 'data:image/png;base64,' + six.moves.urllib.parse.quote(base64.b64encode(imgdata.buf))
+    msg = '<html><img src = "%s"/>' % uri
 
-  area = sum(yd)/(1.0*len(xd))
-  msg += '<p>Area=%s, expected area=%s</p></html>' % (area, options)
-  ok = abs(area-float(options))<0.001
+    area = sum(yd) / (1.0 * len(xd))
+    msg += '<p>Area=%s, expected area=%s</p></html>' % (area, options)
+    ok = abs(area - float(options)) < 0.001
 
-  return dict(ok=bool(ok), msg=msg)
+    return dict(ok=bool(ok), msg=msg)
--- policies/edx4edx/checkjson.py
+++ policies/edx4edx/checkjson.py
@@ -4,10 +4,10 @@ import json
 
 
 def check(fn):
-    print "checking %s" % fn
+    print("checking %s" % fn)
     try:
         json.loads(open(fn).read())
-        print "Ok"
+        print("Ok")
     except Exception as err:
         raise
 
